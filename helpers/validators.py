50ee1cbd8895a76a06483250634f13d9736b6f66
TiagoGIM <herique.sa@gmail.com>
2023-05-23 18:26:57 +0100

    created decorator to valid request fields

---
+++ helpers/validators.py
@@ -0,0 +1,42 @@
+from functools import wraps
+import json
+from flask import abort, make_response, request
+from sqlalchemy import inspect
+
+
+def ValidModel(Model):
+    """
+    Decorator that checks if the required fields for the model were provided in the request,
+    otherwise returns an error message with the list of missing required fields.
+    """
+    def get_required_fields():
+        required_fields = []
+        for column in Model.__table__.columns:
+            if not column.nullable and not column.primary_key:
+                required_fields.append(column.name)
+        return required_fields
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                if request.method == 'POST':
+                    json_data = json.loads(request.data)
+                    if not json_data:
+                        return make_response(f'body must contain JSON with this fields : {", ".join(get_required_fields())}', 400)
+
+                    if not all(key in inspect(Model).columns for key in json_data.keys()):
+                        extra_keys = ", ".join(
+                            set(json_data.keys()) - set(inspect(Model).columns.keys()))
+                        return make_response(f'JSON contains extra fields: {extra_keys}.', 400)
+                    required_fields = get_required_fields()
+                    missing_fields = set(required_fields) - \
+                        set(json_data.keys())
+                    if missing_fields:
+                        return make_response(f'Required fields not provided: {", ".join(missing_fields)}.', 400)
+
+                    return make_response(func(*args, **kwargs))
+            except json.JSONDecodeError as e:
+                return make_response(f'body must contain JSON with this fields : {", ".join(get_required_fields())}', 400)
+        return wrapper
+    return decorator
